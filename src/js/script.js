57bc06db6786ef19375e66ac4ac659af5d69fb9e
junyahaga <n0812034@yahoo.co.jp>
2024-06-26 17:23:38 +0900

    2024/6/26

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,12 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+    document.querySelectorAll('.faq-question').forEach(item => {
+        item.addEventListener('click', () => {
+            const answer = item.nextElementSibling;
+            const icon = item.querySelector('.faq-icon');
+            answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+            icon.classList.toggle('open');
+        });
+    });
 
 });
